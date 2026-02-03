4d5ddbb7b12a33354a3d7f7178ecae14737a1c2c
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-02-03 22:24:37 +0100

    f

---
+++ README.md
@@ -0,0 +1,42 @@
+# Broca
+
+Mnemonic derivation (BIP-39, Monero) for TypeScript
+
+```bash
+npm install @hazae41/broca
+```
+
+[**📦 NPM**](https://www.npmjs.com/package/@hazae41/broca)
+
+## Features
+
+### Current features
+- 100% TypeScript and ESM
+- No external dependencies
+- Rust-like patterns
+- Uses WebCrypto
+- BIP-39, Monero
+
+## Usage
+
+### BIP-39
+
+```tsx
+const mnemonic = await BitcoinSeedPhrase.generate(256)
+
+if (!BitcoinSeedPhrase.validate(mnemonic))
+  throw new Error("Invalid mnemonic seed phrase")
+
+const seed = await BitcoinSeedPhrase.derive(mnemonic)
+```
+
+### Monero
+
+```tsx
+const mnemonic = await MoneroSeedPhrase.generate(256)
+
+if (!MoneroSeedPhrase.validate(mnemonic))
+  throw new Error("Invalid mnemonic seed phrase")
+
+const seed = await MoneroSeedPhrase.derive(mnemonic)
+```
---
+++ src/mods/broca/monero/mod.ts
@@ -0,0 +1,46 @@
+import { Lengthed } from "@/libs/lengthed/mod.ts";
+import { english } from "@/libs/wordlists/english/mod.ts";
+
+export namespace MoneroSeedPhrase {
+
+  export async function generate(strength: 128 | 160 | 192 | 224 | 256, wordlist = english) {
+    const source8 = crypto.getRandomValues(new Uint8Array(strength / 8))
+    const source2 = Array.from(source8).map(b => b.toString(2).padStart(8, "0")).join("")
+
+    const digest8 = new Uint8Array(await crypto.subtle.digest("SHA-256", source8))
+    const digest2 = Array.from(digest8).map(b => b.toString(2).padStart(8, "0")).join("")
+
+    const concat2 = source2 + digest2.slice(0, strength / 32)
+
+    const words: string[] = []
+
+    for (let i = 0; i < concat2.length; i += 11)
+      words.push(wordlist[parseInt(concat2.slice(i, i + 11), 2) % wordlist.length])
+
+    return words.join(" ")
+  }
+
+  export function validate(mnemonic: string, wordlist = english) {
+    const words = mnemonic.trim().normalize("NFKD").split(/\s+/)
+
+    if (![12, 15, 18, 21, 24].includes(words.length))
+      return false
+    if (!words.every(word => wordlist.includes(word)))
+      return false
+
+    return true
+  }
+
+  export async function derive(mnemonic: string, passphrase = "") {
+    const raw = mnemonic.trim().normalize("NFKD").split(/\s+/).join(" ")
+
+    const sal = new TextEncoder().encode("mnemonic" + passphrase)
+    const key = new TextEncoder().encode(raw)
+
+    const alg = { name: "PBKDF2", hash: "SHA-512", salt: sal, iterations: 2048 }
+    const ref = await crypto.subtle.importKey("raw", key, { name: "PBKDF2" }, false, ["deriveBits"])
+
+    return new Uint8Array(await crypto.subtle.deriveBits(alg, ref, 512)) as Uint8Array<ArrayBuffer> & Lengthed<64>
+  }
+
+}
